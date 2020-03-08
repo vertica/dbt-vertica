82cd3ecc6871347de5bbe97f2eca8d60c176728e
Matthew Carter <carter.matt.p@gmail.com>
2020-03-08 00:40:01 -0500

    Switching materialization from view to table error
    When you change materialization for deployed view to a table,
    dbt attempts to rename original view with ALTER TABLE.
    https://github.com/fishtown-analytics/dbt/issues/2161

--- dbt/include/vertica/macros/adapters.sql
+++ dbt/include/vertica/macros/adapters.sql
@@ -121,3 +121,15 @@
   );
 
 {% endmacro %}
+
+{% macro vertica__create_table_as(temporary, relation, sql) -%}
+  {%- set sql_header = config.get('sql_header', none) -%}
+
+  {{ sql_header if sql_header is not none }}
+
+  create {% if temporary: -%}temporary{%- endif %} table
+    {{ relation.include(database=(not temporary), schema=(not temporary)) }}
+  as (
+    {{ sql }}
+  );
+{% endmacro %}
