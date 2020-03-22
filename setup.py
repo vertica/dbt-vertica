ef6cfd452f97eb2b77f62269be6a2f7795d3bb56
Matthew Carter <carter.matt.p@gmail.com>
2020-03-22 00:46:52 -0400

    Update setup to publish to PyPi

--- setup.py
+++ setup.py
@@ -1,16 +1,22 @@
 #!/usr/bin/env python
 from setuptools import find_packages
 from setuptools import setup
+import pathlib
 
 package_name = "dbt-vertica"
 package_version = "0.15.0"
 description = """The vertica adapter plugin for dbt (data build tool)"""
 
+HERE = pathlib.Path(__file__).parent
+README = (HERE / "README.md").read_text()
+
 setup(
     name=package_name,
     version=package_version,
     description=description,
-    long_description=description,
+    long_description=README,
+    long_description_content_type='text/markdown',
+    license='MIT',
     author='Matthew Carter',
     author_email='carter.matt.p@gmail.com',
     url='https://github.com/mpcarter/dbt-vertica',
@@ -19,6 +25,7 @@ setup(
         'dbt': [
             'include/vertica/dbt_project.yml',
             'include/vertica/macros/*.sql',
+            'include/vertica/macros/materializations/*.sql',
         ]
     },
     install_requires=[
