7bd45d52d4dbe85366b07f6dddbd3173e0f34e9b
Stanislav Lysikov <s.lysikov@space307.com>
2022-03-19 18:55:36 +0300

    add params for create schema

--- dbt/include/vertica/macros/materializations/models/table/create_table_as.sql
+++ dbt/include/vertica/macros/materializations/models/table/create_table_as.sql
@@ -1,5 +1,8 @@
 {% macro vertica__create_table_as(temporary, relation, sql) -%}
   {%- set sql_header = config.get('sql_header', none) -%}
+  {%- set order_by_string = config.get('order_by_string', default=none) -%}
+  {%- set segmented_by_string = config.get('segmented_by_string', default=none) -%}
+  {%- set partition_by_string = config.get('partition_by_string', default=none) -%}
 
   {{ sql_header if sql_header is not none }}
 
@@ -8,5 +11,17 @@
     {% if temporary: -%}on commit preserve rows{%- endif %}
   as (
     {{ sql }}
-  );
+  )
+  {% if order_by_string is not none and not temporary -%}
+  order by {{ order_by_string }}
+  {% endif %}
+  {% if segmented_by_string is not none and order_by_string is not none and not temporary -%}
+  segmented by {{ segmented_by_string }}
+  all nodes ksafe{% if database is not none and "docker" in database %} 0 {% else %} 1 {% endif %}
+  {% endif %}
+  {% if partition_by_string is not none and segmented_by_string is not none and not temporary -%}
+  partition by {{ partition_by_string }}
+  group by {{ partition_by_string }}
+  {% endif %}
+  ;
 {% endmacro %}
