d7ba83e7d4e27768916ef84baa9f730931e661ee
Stanislav Lysikov <s.lysikov@space307.com>
2022-04-27 17:45:33 +0300

    template

--- dbt/include/vertica/macros/materializations/models/table/create_table_as.sql
+++ dbt/include/vertica/macros/materializations/models/table/create_table_as.sql
@@ -1,8 +1,12 @@
 {% macro vertica__create_table_as(temporary, relation, sql) -%}
   {%- set sql_header = config.get('sql_header', none) -%}
-  {%- set order_by_string = config.get('order_by_string', default=none) -%}
+  {%- set order_by = config.get('order_by', default=none) -%}
   {%- set segmented_by_string = config.get('segmented_by_string', default=none) -%}
+  {%- set segmented_by_all_nodes = config.get('segmented_by_all_nodes', default=True) -%}
   {%- set partition_by_string = config.get('partition_by_string', default=none) -%}
+  {%- set partition_by_group_by_string = config.get('partition_by_group_by_string', default=none) -%}
+  {%- set partition_by_active_count = config.get('partition_by_active_count', default=none) -%}
+  {%- set ksafe = config.get('ksafe', default=none) -%}
 
   {{ sql_header if sql_header is not none }}
 
@@ -12,16 +16,22 @@
   as (
     {{ sql }}
   )
-  {% if order_by_string is not none and not temporary -%}
-  order by {{ order_by_string }}
+  {% if order_by is not none -%}
+  order by {{ order_by | join(',') }}
   {% endif %}
-  {% if segmented_by_string is not none and order_by_string is not none and not temporary -%}
-  segmented by {{ segmented_by_string }}
-  all nodes ksafe{% if database is not none and "docker" in database %} 0 {% else %} 1 {% endif %}
+  {% if segmented_by_string is not none -%}
+  segmented by {{ segmented_by_string }} {% if segmented_by_all_nodes %} ALL NODES {% endif %}
+  {% if ksafe is not none -%}
+  all nodes ksafe{% if database is not none and "docker" in database %} 0 {% else %} {{ ksafe }} {% endif %}
   {% endif %}
-  {% if partition_by_string is not none and segmented_by_string is not none and not temporary -%}
+  {% if partition_by_string is not none -%}
   partition by {{ partition_by_string }}
-  group by {{ partition_by_string }}
+  {% if partition_by_group_by_string is not none -%}
+  group by {{ partition_by_group_by_string }}
+  {% endif %}
+  {% if partition_by_active_count is not none -%}
+  ACTIVEPARTITIONCOUNT {{ partition_by_active_count }}
+  {% endif %}
   {% endif %}
   ;
 {% endmacro %}
