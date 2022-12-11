f76e1518f89bed91c9b6d02e8de6ac0afef35e86
Alexey Kubrakov <a.kubrakov@dataduck.com>
2022-12-12 00:30:37 +0300

    fix: create_table_as.sql compilation error
    Fixed nesting typo (endif was missing)
    
    Removed "is not None" since
    there is only "none" jinja test available
    thus compilation error was still the issue
    
    Added pytest output filter

--- dbt/include/vertica/macros/materializations/models/table/create_table_as.sql
+++ dbt/include/vertica/macros/materializations/models/table/create_table_as.sql
@@ -8,8 +8,8 @@
   {%- set partition_by_group_by_string = config.get('partition_by_group_by_string', default=None) -%}
   {%- set partition_by_active_count = config.get('partition_by_active_count', default=None) -%}
   {%- set ksafe = config.get('ksafe', default=None) -%}
-  
-  {{ sql_header if sql_header is not None }}
+
+  {{ sql_header if sql_header }}
 
   create {% if temporary: -%}local temporary{%- endif %} table
     {{ relation.include(database=(not temporary), schema=(not temporary)) }}
@@ -17,23 +17,24 @@
   as (
     {{ sql }}
   )
-  {% if order_by is not None -%}
-  order by {{ order_by | join(',') }}
+  {% if order_by -%}
+    order by {{ order_by | join(',') }}
   {% endif %}
-  {% if segmented_by_string is not None -%}
-  segmented by {{ segmented_by_string }} {% if segmented_by_all_nodes %} ALL NODES {% endif %}
+  {% if segmented_by_string -%}
+    segmented by {{ segmented_by_string }} {% if segmented_by_all_nodes %} ALL NODES {% endif %}
+  {% endif %}
   {% if no_segmentation %}UNSEGMENTED ALL NODES{% endif %}
-  {% if ksafe is not None -%}
-  ksafe {{ ksafe }}
-  {% endif %}
-  {% if partition_by_string is not None -%}
-  partition by {{ partition_by_string }}
-  {% if partition_by_string is not None and partition_by_group_by_string is not None -%}
-  group by {{ partition_by_group_by_string }}
-  {% endif %}
-  {% if partition_by_string is not None and partition_by_active_count is not None -%}
-  ACTIVEPARTITIONCOUNT {{ partition_by_active_count }}
-  {% endif %}
+    {% if ksafe -%}
+      ksafe {{ ksafe }}
+    {% endif %}
+    {% if partition_by_string -%}
+      partition by {{ partition_by_string }}
+    {% if partition_by_string and partition_by_group_by_string -%}
+      group by {{ partition_by_group_by_string }}
+    {% endif %}
+    {% if partition_by_string and partition_by_active_count -%}
+      ACTIVEPARTITIONCOUNT {{ partition_by_active_count }}
+    {% endif %}
   {% endif %}
   ;
 {% endmacro %}
