39684f219218cafdd4bc6c4be974a0d14a5f527d
Stanislav Lysikov <s.lysikov@space307.com>
2023-03-22 14:18:52 +0300

    remove additional table params for temp tables

--- dbt/include/vertica/macros/materializations/models/table/create_table_as.sql
+++ dbt/include/vertica/macros/materializations/models/table/create_table_as.sql
@@ -9,7 +9,7 @@
   {%- set partition_by_string = config.get('partition_by_string', default=none) -%}
   {%- set partition_by_group_by_string = config.get('partition_by_group_by_string', default=none) -%}
   {%- set partition_by_active_count = config.get('partition_by_active_count', default=none) -%}
-  
+
   create {% if temporary: -%}local temporary{%- endif %} table
     {{ relation.include(database=(not temporary), schema=(not temporary)) }}
     {% if temporary: -%}on commit preserve rows{%- endif %}
@@ -17,31 +17,32 @@
     {{ sql }}
   )
 
-  {% if order_by is not none  -%}
-      order by {{ order_by }} 
-  {% endif -%}
+  {% if not temporary: %}
+    {% if order_by is not none  -%}
+        order by {{ order_by }}
+    {% endif -%}
 
-  {% if segmented_by_string is not none -%}
-              segmented  BY  {{ segmented_by_string }} {% if segmented_by_all_nodes %} ALL NODES {% endif %}
+    {% if segmented_by_string is not none -%}
+                segmented  BY  {{ segmented_by_string }} {% if segmented_by_all_nodes %} ALL NODES {% endif %}
+    {% endif %}
+
+    {% if no_segmentation =='True' or no_segmentation=='true'  -%}
+      UNSEGMENTED ALL NODES
+    {% endif -%}
+
+    {% if ksafe is not none -%}
+      ksafe {{ ksafe }}
+    {% endif -%}
+
+    {% if partition_by_string is not none -%}
+      ; alter table {{ relation.include(database=(not temporary), schema=(not temporary)) }} partition BY {{ partition_by_string }}
+      {% if partition_by_string is not none and partition_by_group_by_string is not none -%}
+        group by {{ partition_by_group_by_string }}
+      {% endif %}
+      {% if partition_by_string is not none and partition_by_active_count is not none %}
+        SET ACTIVEPARTITIONCOUNT {{ partition_by_active_count }}
+      {% endif %}
+    {% endif %}
   {% endif %}
-
-  {% if no_segmentation =='True' or no_segmentation=='true'  -%} 
-    UNSEGMENTED ALL NODES 
-  {% endif -%}
-
-  {% if ksafe is not none -%}
-    ksafe {{ ksafe }}
-  {% endif -%}
-  
-  {% if partition_by_string is not none -%}
-    ; alter table {{ relation.include(database=(not temporary), schema=(not temporary)) }} partition BY {{ partition_by_string }}
-    {% if partition_by_string is not none and partition_by_group_by_string is not none -%}
-      group by {{ partition_by_group_by_string }}
-    {% endif %}
-    {% if partition_by_string is not none and partition_by_active_count is not none %}
-      SET ACTIVEPARTITIONCOUNT {{ partition_by_active_count }}
-    {% endif %}
-  {% endif %}  
   ;
 {% endmacro %}
-
