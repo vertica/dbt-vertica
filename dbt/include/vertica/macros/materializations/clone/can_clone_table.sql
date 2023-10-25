e344da38dbcb0c312e84687bead29968d6d91742
sachinthakur96 <sachin.thakur@simplify3x.com>
2023-10-25 17:13:43 +0530

    adding clone

--- dbt/include/vertica/macros/materializations/clone/can_clone_table.sql
+++ dbt/include/vertica/macros/materializations/clone/can_clone_table.sql
@@ -4,6 +4,5 @@
     {{ return(adapter.dispatch('can_clone_table', 'dbt')()) }}
 {% endmacro %}
 
-{% macro default__can_clone_table() %}
-    {{ return(False) }}
-{% endmacro %}
+
+
--- dbt/include/vertica/macros/materializations/clone/create_or_replace_clone.sql
+++ dbt/include/vertica/macros/materializations/clone/create_or_replace_clone.sql
@@ -2,6 +2,3 @@
     {{ return(adapter.dispatch('create_or_replace_clone', 'dbt')(this_relation, defer_relation)) }}
 {% endmacro %}
 
-{% macro default__create_or_replace_clone(this_relation, defer_relation) %}
-    create or replace table {{ this_relation }} clone {{ defer_relation }}
-{% endmacro %}
