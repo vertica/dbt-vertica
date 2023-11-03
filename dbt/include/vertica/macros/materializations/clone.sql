019f146ac04ab38998e6484789876ad164706609
sachinthakur96 <sachin.thakur@simplify3x.com>
2023-11-03 16:00:19 +0530

    Adding

--- dbt/include/vertica/macros/materializations/clone.sql
+++ dbt/include/vertica/macros/materializations/clone.sql
@@ -1,13 +1,15 @@
 
-
-
 {% macro vertica__can_clone_table() %}
     {{ return(True) }}
 {% endmacro %}
 
 
 {% macro vertica__create_or_replace_clone(this_relation, defer_relation) %}
+
+
     create table
-        {{ this_relation }} as select * from  {{ defer_relation }}
-     
+        {{ this_relation }} as select * from
+      {{ defer_relation }}
+
+      
 {% endmacro %}
