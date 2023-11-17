00abf9d11cbb98b3c5836be539dd6fab38e14cf2
sachinthakur96 <sachin.thakur@simplify3x.com>
2023-11-17 18:05:04 +0530

    Adding

--- tests/functional/adapter/ephemeral/test_ephemeral.py
+++ tests/functional/adapter/ephemeral/test_ephemeral.py
@@ -17,23 +17,23 @@
 
 
 import pytest
-# from dbt.tests.adapter.ephemeral.test_ephemeral import BaseEphemeralMulti
-# from dbt.tests.util import run_dbt, check_relations_equal
+from dbt.tests.adapter.ephemeral.test_ephemeral import BaseEphemeralMulti
+from dbt.tests.util import run_dbt, check_relations_equal
 
-# class TestEphemeralMultiVertica(BaseEphemeralMulti):
+class TestEphemeralMultiVertica(BaseEphemeralMulti):
 
-#     def test_ephemeral_multi_snowflake(self, project):
-#         run_dbt(["seed"])
-#         results = run_dbt(["run"])
-#         assert len(results) == 3
-#         # check_relations_equal(project.adapter, ["SEED", "DEPENDENT", "DOUBLE_DEPENDENT", "SUPER_DEPENDENT"])
-#         # check_relations_equal(project.adapter, ["seed", "dependent", "double_dependent", "super_dependent"])
-#         # check_relations_equal(project.adapter, ["seed", "dependent", "double_dependent", "super_dependent"])
+    def test_ephemeral_multi_snowflake(self, project):
+        run_dbt(["seed"])
+        results = run_dbt(["run"])
+        assert len(results) == 3
+        # check_relations_equal(project.adapter, ["SEED", "DEPENDENT", "DOUBLE_DEPENDENT", "SUPER_DEPENDENT"])
+        # check_relations_equal(project.adapter, ["seed", "dependent", "double_dependent", "super_dependent"])
+        # check_relations_equal(project.adapter, ["seed", "dependent", "double_dependent", "super_dependent"])
      
  
-#         check_relations_equal(project.adapter, ["seed", "dependent"])
-#         check_relations_equal(project.adapter, ["seed", "double_dependent"])
-#         check_relations_equal(project.adapter, ["seed", "super_dependent"])
+        check_relations_equal(project.adapter, ["seed", "dependent"])
+        check_relations_equal(project.adapter, ["seed", "double_dependent"])
+        check_relations_equal(project.adapter, ["seed", "super_dependent"])
         
 
 
