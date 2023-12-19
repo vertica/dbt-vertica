73533c8450f4854a067a869142dbe418e6612889
sachinthakur96 <sachin.thakur@simplify3x.com>
2023-12-19 13:17:35 +0530

    Adding

--- tests/functional/adapter/utils/test_new_test_case.py
+++ tests/functional/adapter/utils/test_new_test_case.py
@@ -3,25 +3,66 @@
 
 from dbt.tests.adapter.utils.test_date_spine import BaseDateSpine
 
-from dbt.tests.adapter.utils.test_get_intervals_between import BaseGetIntervalsBetween 
+# from dbt.tests.adapter.utils.test_get_intervals_between import BaseGetIntervalsBetween 
 from dbt.tests.adapter.utils.test_generate_series import BaseGenerateSeries 
 
 from dbt.tests.adapter.utils.test_get_powers_of_two import BaseGetPowersOfTwo
 
 
+
+import pytest
+from dbt.tests.adapter.utils.base_utils import BaseUtils
+
+models__test_get_intervals_between_sql = """
+SELECT
+  {% if target.type == 'postgres' %}
+    {{ get_intervals_between('day',"'09/01/2023'::date", "'09/12/2023'::date" ) }} as intervals,
+  {% else %}
+      {{ get_intervals_between( "'09/01/2023' ::date", "'09/12/2023' ::date",'day'  ) }} as intervals,
+  {% endif %}
+  11 as expected
+
+"""
+
+models__test_get_intervals_between_yml = """
+version: 2
+models:
+  - name: test_get_intervals_between
+    tests:
+      - assert_equal:
+          actual: intervals
+          expected: expected
+"""
+
+
+class BaseGetIntervalsBetween(BaseUtils):
+    @pytest.fixture(scope="class")
+    def models(self):
+        return {
+            "test_get_intervals_between.yml": models__test_get_intervals_between_yml,
+            "test_get_intervals_between.sql": self.interpolate_macro_namespace(
+                models__test_get_intervals_between_sql, "get_intervals_between"
+            ),
+        }
+
+
+
+
+
+
 class TestDateSpine(BaseDateSpine):
     pass
 
-#pass
+# pass
 class TestGenerateSeries(BaseGenerateSeries):
     pass
 
 
-#pass
+# #pass
 class TestGetPowersOfTwo(BaseGetPowersOfTwo):
     pass
 
 
 
-class Test1GetIntervalsBeteween(BaseGetIntervalsBetween):
+class TestGetIntervalsBeteween(BaseGetIntervalsBetween):
     pass
