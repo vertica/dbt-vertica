031ab1caf2a913dcaa29b679a735cbcdf76e2749
sachinthakur96 <sachin.thakur@simplify3x.com>
2023-12-22 12:57:10 +0530

    adding version 1.7.3

--- setup.py
+++ setup.py
@@ -78,7 +78,7 @@ def _get_dbt_core_version():
 
 
 package_name = "dbt-vertica"
-package_version = "1.7.0"
+package_version = "1.7.3"
 description = """Official vertica adapter plugin for dbt (data build tool)"""
 dbt_core_version = _get_dbt_core_version()
 
