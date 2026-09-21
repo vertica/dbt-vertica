6b4a2f16d58975bf50358c150a6e360ed258e446
ajay-abrol2 <ajay.abrol2@microfocus.com>
2026-09-21 12:37:44 +0530

    Fixed the review comments

--- tests/conftest.py
+++ tests/conftest.py
@@ -78,7 +78,7 @@ def vertica_target():
         'threads': 1,
         'host': os.getenv('VERTICA_HOST', 'verticadb-sample'),
         'username': os.getenv('VERTICA_USER', 'dbadmin'),
-        'password': os.getenv('VERTICA_PASSWORD', 'Vertica@123'),
+        'password': os.getenv('VERTICA_PASSWORD', ''),
         'database': os.getenv('VERTICA_DATABASE', 'vdb'),
         'port': int(os.getenv('VERTICA_PORT', '5433')),
     }
